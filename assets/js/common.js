7e34116d27b9e3a869d7499491f74669f70dddb6
Yukii96 <1216595196@qq.com>
2020-06-19 10:34:14 +0800

    首页和登录注册页面结束

--- assets/js/common.js
+++ assets/js/common.js
@@ -2,6 +2,11 @@
 var baseURL = 'http://ajax.frontend.itheima.net/'
 $.ajaxPrefilter(function(option) {
     // option形参是jQuery请求方法的配置信息
+    // 4. 请求发送之前触发beforeSend
+    option.beforeSend = function() {
+        // 4.1 发送请求之前开始进度条（在引入NProgress插件的情况下才会执行后面的方法）
+        NProgress && NProgress.start()   //短路运算
+    }
     // 1. 配置通用的url地址
     option.url = baseURL + option.url
     // 2. 设置接口的请求头
@@ -10,7 +15,9 @@ $.ajaxPrefilter(function(option) {
     option.headers = { Authorization: localStorage.getItem('mytoken')}
     // 3. 处理通用的异常情况（服务器返回来的有异常信息）
     option.complete = function(res) {
-        // 服务器响应结束时触发，相当于success
+        // 服务器响应结束时触发complete，相当于success
+        // 4.2 完成请求后结束进度条（在引入NProgress插件的情况下才会执行后面的方法）
+        NProgress && NProgress.done()
         // 处理失败的情况
         if (res.responseJSON.status === 1 && res.responseJSON.message === '身份认证失败！') {
             // 如果身份验证失败了，把无效的token清除，就跳转到登录页
