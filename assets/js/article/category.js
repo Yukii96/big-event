4296c8fb7dcf9dd711442675c3632b20c167acb1
Yukii96 <1216595196@qq.com>
2020-06-20 14:32:44 +0800

    文章分类

--- assets/js/article/category.js
+++ assets/js/article/category.js
@@ -1,5 +1,17 @@
 $(function() {
     
-    // 1. 
+    // 1. 加载数据分类数据
+    function loadListData () {
+        $.ajax({
+            type: 'get',
+            url: 'my/article/cates',
+            success: function(res) {
+                // 2. 基于模板引擎做渲染
+                var result = template('list-tpl',res)
+                $('.layui-table tbody').html(result)
+            }
+        })
+    }
+    loadListData()
 
 })
